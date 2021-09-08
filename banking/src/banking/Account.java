4b2f262fafb1507affc54fdd0b7a93f5086e2cf2
chicdd <chicdd02@gmail.com>
2021-09-08 20:04:54 +0900

    banking

---
+++ banking/src/banking/Account.java
@@ -0,0 +1,48 @@
+package banking;
+
+public class Account {
+	private String ano;   //계좌 번호
+	private String owner; //계좌주
+	private int balance;  //잔고
+
+	
+	public Account(String ano, String owner, int balance) {
+		this.ano = ano;// 초기화
+		this.owner = owner;
+		this.balance = balance;
+	}
+
+	//get(),set() 메서드
+	public String getAno() {
+		return ano;
+	}
+
+
+	public void setAno(String ano) {
+		this.ano = ano;
+	}
+
+
+	public String getOwner() {
+		return owner;
+	}
+
+
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+
+
+	public int getBalance() {
+		return balance;
+	}
+
+
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+	
+	
+	
+	
+}
